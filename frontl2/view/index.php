a7d248974ec40b6a6c33ca852a3b0453ba338afd
agailevictor <agailevictor@gmail.com>
2016-07-04 10:13:55 +0530

    Signed-off-by: agailevictor <agailevictor@gmail.com>

---
+++ frontl2/view/index.php
@@ -0,0 +1,72 @@
+<!-- to use scroller : class="col-md-12 scroll_set_1" -->
+<?php include 'template/default_header.php' ?>
+<title>CEODB L2</title>
+<div id="wrapper" class="">
+	<div id="wrapper_landing_page">
+		<!-- -- -->
+		
+		
+		<!-- -- -->
+		<!-- header -->
+		<div id="header">
+			<div class="col-md-12">
+				<div class="col-md-3">
+					<div class="logo_holder col-md-12">
+						<div class="logo"><img src="../assets/images/logo/ceodb-logo-bw-sm.png"></div>
+						<div class="logo"><img src="../assets/images/logo/mpxd_md.png"></div>
+					</div>
+				</div>
+				<div class="col-md-9">
+					<div class="row">
+						<div class="col-md-3">
+						</div>
+						<div id="header_menu" class="col-md-9">
+							<span class="hm_button active"><i class="fa fa-home" aria-hidden="true"></i> Home</span>
+							<span class="hm_button"><i class="fa fa-line-chart" aria-hidden="true"></i> S-Curve</span>
+							<span class="hm_button"><i class="fa fa-cube" aria-hidden="true"></i> Procurement</span>
+							<span class="hm_button"><i class="fa fa-money" aria-hidden="true"></i> Commercial</span>
+							<span class="hm_button"><i class="fa fa-sign-in" aria-hidden="true"></i> Logout</span>
+							<span class="hm_user_profile">
+								<i class="fa fa-user" aria-hidden="true"></i> Profile 
+								<i class="fa fa-caret-down" aria-hidden="true"></i>
+							</span>
+						</div>
+					</div>
+				</div>
+				<div id="header_datetime">
+					<span class="hd_label hd_label_today"><i class="fa fa-calendar-o" aria-hidden="true"></i> Today: 22 JUN 2016</span>
+					<span class="hd_label hd_label_data"><i class="fa fa-calendar-check-o" aria-hidden="true"></i> Data Date : 14 MAY 2016</span>
+					<span class="hd_button">Change</span>
+				</div>
+			</div>
+		</div>
+		<!-- -- -->
+		
+		<!-- -- -->
+		<!-- content -->
+		<div id="content">
+			<div id="dashboard" class="col-md-12">
+				
+				<div id="infographic" class="">
+					<div class="ig_plate">
+						<!-- -- -->
+						
+						<?php include 'module/gis_accessories.php' ?>
+						
+						<!-- -- -->
+						<img src="../assets/images/base/MRT-L2-1080.jpg">
+					</div>
+					<div class="ig_plate">
+						<img src="../assets/images/base/MRT-L2-1080.png">
+					</div>
+				</div>
+			</div>
+		</div>
+		<!-- -- -->
+			
+				
+		
+		<!-- -- -->
+	</div>
+</div>
+<?php include 'template/default_footer.php' ?>
---
+++ frontl2/view/module/gis_accessories.php
@@ -0,0 +1,164 @@
+<div id="float_plate" class="row">
+	<div id="plate_vector_map" class="plate_vector_map">
+		missing plate_vector_map.svg
+	</div>
+	<div id="plate_overall_progress">
+		<div id="pop_label">
+			<span class="set1">Overall Progress</span>
+			<span class="set2">Actual</span>
+			<span class="set3">Planned</span>
+			<span class="set4">Variance</span>
+			<span class="set5">Trend</span>
+		</div>
+		<div id="pop_value">
+			<span class="set1">76%</span>
+			<span class="set2">78%</span>
+			<span class="set3">-2w</span>
+			<span class="set4"><i class="fa fa-arrow-up" aria-hidden="true"></i> UP</span>
+		</div>
+	</div>
+	<div id="plate_commercial">
+		<div id="pcom_label">
+			<span class="set1">Project Cost Update</span>
+			<span class="set2">Project Spend To Date</span>
+			<span class="set3">Awarded Package</span>
+			<span class="set4">PDP Reimbursables</span>
+			<span class="set5">WPCS Payment</span>
+			<span class="set6">Retention Sum</span>
+			<span class="set7">Variation Orders</span>
+			<span class="set8">Contigency Sum</span>
+		</div>
+		<div id="pcom_value">
+			<span class="set1">16.75 Bil</span>
+			<span class="set2">21.52 Bil</span>
+			<span class="set3">1.55 Mil</span>
+			<span class="set4">13.50 Bil</span>
+			<span class="set5">382.51 Mil</span>
+			<span class="set6">502.80 Mil</span>
+			<span class="set7">775.46 Mil</span>
+		</div>
+	</div>
+	<div id="plate_legend">
+		<div id="pl_label_header">
+			<span class="set1">Legends</span>
+		</div>
+		<div id="pl_label_content">
+			<span class="set1">Not Started</span>
+			<span class="set2">On Schedule</span>
+			<span class="set3">Behind Late</span>
+			<span class="set4">Critical</span>
+		</div>
+	</div>
+	<!-- <div id="plate_issue">
+		<div id="pi_title">
+			<span class="set1"><i class="fa fa-exclamation-triangle" aria-hidden="true"></i></span>
+			<span class="set2">Issues</span>
+		</div>
+		<div id="pi_subtitle">
+			<span class="set1">Click to view</span>
+		</div>
+	</div> -->
+			
+			
+			
+	<div id="plate_system">
+		<div id="donut_plate" style="z-index: 3000;">
+			<div id="dp_db_donut_0" class="dp_top"></div>
+			<div id="dp_db_donut_1" class="dp_bottom"></div>
+			<div id="dp_db_donut_2" class="dp_bottom"></div>
+			<div id="dp_db_donut_3" class="dp_bottom"></div>
+			<div id="dp_db_donut_4" class="dp_bottom"></div>
+			<div id="dp_db_donut_5" class="dp_bottom"></div>
+			<div id="dp_db_donut_6" class="dp_bottom"></div>
+			<div id="dp_db_donut_7" class="dp_bottom"></div>
+		</div>
+	</div>
+	
+	<div id="plate_physical">
+		<div id="pp_station">
+			<span class="set1"></span>
+		</div>
+		<div id="pp_depot">
+			<span class="set1"></span>
+		</div>
+	</div>
+	<div id="plate_label">
+		<div id="pl_depot">
+			<span class="set1">Serdang Depot</span>
+		</div>
+		<div id="pl_station">
+			<span class="set1">Sg. Buloh</span>
+			<span class="set2">Damansara Damai</span>
+			<span class="set3">Sri Damansara West</span>
+			<span class="set4">Sri Damansara East</span>
+			<span class="set5">Kepong Sentral</span>
+			<span class="set6">Metro Prima</span>
+			<span class="set7">Kepong Baru</span>
+			<span class="set8">Jinjang</span>
+			<span class="set9">Sri Delima</span>
+			<span class="set10">Kg. Batu</span>
+			<span class="set11">Kentonmen</span>
+			<span class="set12">Jln. Ipoh</span>
+			<span class="set13">Sentul West</span>
+			<span class="set14">Titiwangsa</span>
+			<span class="set15">HKL</span>
+			<span class="set16">Kg. Baru North</span>
+			<span class="set17">Ampang Park</span>
+			<span class="set18">KLCC East</span>
+			<span class="set19">Conley</span>
+			<span class="set20">TRX</span>
+			<span class="set21">Chan Sow Lin</span>
+			<span class="set22">Bdr. Malaysia North</span>
+			<span class="set23">Bdr. Malaysia South</span>
+			<span class="set24">Kuchai Lama</span>
+			<span class="set25">Tmn. Naga Emas</span>
+			<span class="set26">Sg. Besi</span>
+			<span class="set27">Tech. Park</span>
+			<span class="set28">Serdang Raya North</span>
+			<span class="set29">Serdang Raya South</span>
+			<span class="set30">Sri Kembangan</span>
+			<span class="set31">UPM</span>
+			<span class="set32">Tmn. Universiti</span>
+			<span class="set33">Equine Park</span>
+			<span class="set34">Tmn. Putra Permai</span>
+			<span class="set35">16 Sierra</span>
+			<span class="set36">Cyberjaya North</span>
+			<span class="set37">Cyberjaya City Centre</span>
+			<span class="set38">Putrajaya Sentral</span>
+		</div>
+		<div id="pl_mspr">
+			<span class="set1">P</span>
+			<span class="set2">P</span>
+			<span class="set3">P</span>
+			<span class="set4">P</span>
+			<span class="set5">P</span>
+			<span class="set6">P</span>
+			<span class="set7">P</span>
+			<span class="set8">P</span>
+			<span class="set9">P</span>
+			<span class="set10">P</span>
+			<span class="set11">P</span>
+			<span class="set12">P</span>
+			<span class="set13">P</span>
+			<span class="set14">P</span>
+			<span class="set15">P</span>
+		</div>
+		<div id="pl_package">
+            <span style="cursor: pointer" onclick="location.href='/mpxd2/v1/index'" class="set1">V201</span>
+			<span class="set2">V202</span>
+			<span class="set3">V203</span>
+			<span class="set4">UG</span>
+			<span class="set5">V204</span>
+			<span class="set6">V205</span>
+			<span class="set7">V206</span>
+			<span class="set8">V207</span>
+			<span class="set9">V208</span>
+			<span class="set10">V209</span>
+			<span class="set11">V210</span>
+		</div>
+		<div id="pl_phase">
+			<span class="set1">NORTH</span>
+			<span class="set2">SOUTH</span>
+		</div>
+	</div>
+</div>
