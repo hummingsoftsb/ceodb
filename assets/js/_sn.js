53037a51d6b0e548652d73b391d479ebdd74e97b
sebint <sebin@hummingsoft.com.my>
2016-08-22 17:00:21 +0530

    Signed-off-by: sebint <sebin@hummingsoft.com.my>

--- assets/js/_sn.js
+++ assets/js/_sn.js
@@ -26,7 +26,9 @@ mpxd.constructors.chainage_progress = function(data) {
     mpxd.modules.general.GenerateGeneralview(data);
 }
 mpxd.constructors.area_progress = function(data) {
-    mpxd.modules.general.GenerateGeneralview(data);
+    var el = "#portlet_" + data.id;
+    if (typeof callback == "undefined") callback = function(){};
+    return new mpxd.modules.track_works.area({data: data, el: el, callback: callback});
 }
 mpxd.constructors.station_tracklist = function(data,callback) {
     var el = "#portlet_" + data.id;
@@ -1145,6 +1147,17 @@ mpxd.modules.track_works.progress = Backbone.View.extend({
         template = _.template(html, {data: that.data});
         that.$el.html(template);
         RenderPieChart(that.$el.find('#chart_' + that.data.id)[0], 100);
+    }
+});
+mpxd.modules.track_works.area = Backbone.View.extend({
+    initialize: function (options) {
+        this.data = options.data;
+        this.render();
+    },render: function () {
+        var that = this;
+        var html = mpxd.getTemplate(that.data.type);
+        template = _.template(html, {data: that.data});
+        that.$el.html(template);
     }
 });
 function RenderPieChart(elementId, dataList) {
